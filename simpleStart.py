356bfce26ac924563a9b59c8d828d8c70a70ed04
praroh2 <praroh2@gmail.com>
2014-08-30 16:06:51 +0530

    Improved simpleStart

--- simpleStart.py
+++ simpleStart.py
@@ -4,23 +4,26 @@ Do not open or run any file apart from this one.
 
 import sys, os
 sys.path.append(os.getcwd())
+## Do not delete these two lines of code.
+## It is required to access help() easily. (For dev only)
 
-while True:
-    print('0. Exit\n1. Start Test\n2. Set questions manually')
-    print('3. Set questions from file')
-    Lin = input("Enter choice: ")
-    if Lin == '0':
-        print("Goodbye!!!")
-        break
-    elif Lin == '1':
-        import start
-        start.main()
-        break
-    elif Lin == '2':
-        import generateQfileManually
-        generateQfileManually.main()
-        break
-    elif Lin == '3':
-        import generateQfileFromIpFile
-        generateQfileFromIpFile.main()
-        break
+if __name__ == "__main__":
+    while True:
+        print('0. Exit\n1. Start Test\n2. Set questions manually')
+        print('3. Set questions from file')
+        Lin = input("Enter choice: ")
+        if Lin == '0':
+            print("Goodbye!!!")
+            break
+        elif Lin == '1':
+            import start
+            start.main()
+            break
+        elif Lin == '2':
+            import generateQfileManually
+            generateQfileManually.main()
+            break
+        elif Lin == '3':
+            import generateQfileFromIpFile
+            generateQfileFromIpFile.main()
+            break
